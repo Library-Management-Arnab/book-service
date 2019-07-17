1f9f59baf5268125873be13cd258cb44bb7fd6cd
Arnab Mondal <arnab2807@gmail.com>
2019-07-17 21:12:21 +0530

    Initial Commit

---
+++ src/main/java/com/lms/bs/rest/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lms.bs.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages= {"com.jpop.epam"})
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
